0e4ab7d3e8fecd3594d1b91aba4410a93998eacd
mccalljoey <mccalljoey@gmail.com>
2021-01-13 19:44:18 +0000

    moved my inputs to their own coordinated document.

--- play.js
+++ play.js
@@ -1,36 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-const setupInput = function(conn) {
-  connection = conn;
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(data) {
-  // let stdInput = setupInput();
-  // stdInput.on('data', (key) => {
-    switch (data) {
-      case '\u0003' : process.exit();
-      break;
-      case 'w' : connection.write(`Move: up`)
-      break;
-      case 'a' : connection.write(`Move: left`)
-      break;
-      case 's' : connection.write(`Move: down`)
-      break;
-      case 'd' : connection.write(`Move: right`)
-      break;
-      case '\u000D' :connection.write('Say: ' + statement)
-      statement = ''
-      break;
-      default: statement += data;
-        break;
-    } 
-};
+setupInput();
